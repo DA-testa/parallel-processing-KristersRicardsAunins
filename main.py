f69666c989642ff9b815b73c634afc76f601b146
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-13 10:26:11 +0200

    test

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    for i in range(m):
+        laiks = data[i]
+        thread = heapq.heappop(threads)
+        output.append((thread[1], thread[0]))
+        heapq.heappush(threads, (thread[0] + laiks, thread[1]))
 
     return output
 
@@ -13,15 +18,19 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    #n = 0
+    #m = 0
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    #data = []
 
     # TODO: create the function
+    n, m = map(int,input().split())
+    data = list(map(int, input().split()))
     result = parallel_processing(n,m,data)
+    for i in range(m):
+        print(result[i][0], result[i][1])
     
     # TODO: print out the results, each pair in it's own line
 
